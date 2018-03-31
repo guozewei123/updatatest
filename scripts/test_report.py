a68b9a050645276c0f50de6e5a8a8fb79fc317c5
guozewei123 <37658434+guozewei123@users.noreply.github.com>
2018-03-31 16:08:25 +0800

    Update test_report.py

--- scripts/test_report.py
+++ scripts/test_report.py
@@ -5,5 +5,9 @@ class TestReport:
         assert 1
     def test_report2(self):
         print(123)
+        assert 0   
+    def test_report3(self):
+        print(123)
+        assert 1
+    def test_report4(self):
         assert 0
-
